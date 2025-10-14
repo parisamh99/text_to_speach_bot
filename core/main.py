c69c30cd9f9b239846a1453c6030b05b141a6f38
parisamh99 <parisa.mahmoodnia@gmail.com>
2025-10-14 06:40:19 -0700

    create a bot that convert text to voice

---
+++ core/main.py
@@ -0,0 +1,23 @@
+import telebot
+import os 
+import logging
+from gtts import gTTS
+
+API_TOKEN ='8123364174:AAH1Fx4X9hfxBbVL-0LmMS8vB0ZFAPgmsfk'
+
+bot = telebot.TeleBot(API_TOKEN)    
+
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id,"send me a text and i will read it for you")
+
+@bot.message_handler(func=lambda message:True)
+def text_to_speach(message):
+    text = message.text
+    file_path = "voices/output.mp3"
+    audio = gTTS(text=text ,lang="en",tld='com.au')
+    audio.save(file_path)
+    bot.send_voice(chat_id=message.chat.id,reply_to_message_id=message.id,voice=open(file_path,"rb"))
+    os.remove(file_path)
+
+bot.infinity_polling()
